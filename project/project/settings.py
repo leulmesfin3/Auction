beefd9a0f99a816bb7627677e24d6e0fcd780044
leulmesfin3 <leul.mesfin.3@gmail.com>
2023-06-23 07:44:26 +0000

    Testing

--- project/project/settings.py
+++ project/project/settings.py
@@ -19,6 +19,7 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
